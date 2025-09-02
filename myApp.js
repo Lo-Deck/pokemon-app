13a402aa1b88cea5072d611df0456dc63fa2df64
Lo-Deck <lolodeq@gmail.com>
2025-09-02 11:59:19 +0200

    update: app.js

--- myApp.js
+++ myApp.js
@@ -6,13 +6,34 @@ let app = express();
 
 // console.log('Hello World');
 
-app.use(function(req, res, next){
+app.use(function (req, res, next) {
+  // console.log("I'm a middleware...");
+  // console.log(`${req.method}  ${req.path} - ${req.ip}`);
+  var string = req.method + " " + req.path + " - " + req.ip;
+  console.log(string);
 
-    // console.log("I'm a middleware...");
-    console.log(`${req.method}  ${req.path} - ${req.ip}`);
-    next();
-
+  next();
 });
+
+
+
+app.get('/now', function(req, res, next) {
+
+        req.time = new Date().toString();
+
+        next();
+
+    }, function(req, res) {
+
+        res.send({time: req.time});
+        
+    }
+);
+
+
+
+
+
 
 
 app.use('/public', express.static(__dirname + '/public'));
