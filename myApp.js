a4b82e0d4667464e1710125c8e031686b69d3e2c
Lo-Deck <lolodeq@gmail.com>
2025-09-02 09:42:32 +0200

    update: app.js

--- myApp.js
+++ myApp.js
@@ -8,14 +8,14 @@ console.log('Hello World');
 app.use('/public', express.static(__dirname + '/public'));
 
 
-app.get( '/', (req, res) => {
+app.get( '/json', (req, res) => {
 
     // res.send('Hello Express');
-
-
     // absolutePath = __dirname + "/views/index.html";
 
-    res.sendFile(__dirname + "/views/index.html");
+    // res.sendFile(__dirname + "/views/index.html");
+
+    res.json({"message": "Hello json"});
 
 
 
