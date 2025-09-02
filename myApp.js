1db1f00b44aa8f59725748503a68f80e27e0bc31
Lo-Deck <lolodeq@gmail.com>
2025-09-02 11:11:25 +0200

    update: app.js

--- myApp.js
+++ myApp.js
@@ -13,16 +13,16 @@ app.get( '/', (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
 });
 
-
+    console.log(process.env.MESSAGE_STYLE);
 app.get( '/json', (req, res) => {
 
-    console.log(process.env.MESSAGE_STYLE);
+
 
     let message = process.env.MESSAGE_STYLE;
 
 
     if(process.env.MESSAGE_STYLE === 'uppercase'){
-        res.json({"message": message.uppercase()});
+        res.json({"message": message.toUpperCase()});
     }
     else{
         res.json({"message": message});    
